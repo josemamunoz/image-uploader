2ac02856354e0bd5de8d660056f8a30596aa6fa6
josemamunoz <josema.munoz@gmail.com>
2020-12-21 00:29:21 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import {ReactComponent as Logo} from './image.svg';
 
 function App() {
+  const [highlighted, setHighlighted] = useState(false);
+  const [contacts, setContacts] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="md:container md:mx-auto justify-center inline-flex items-center">
+
+      <div className="box-border md:box-content h-50 w-50 border font-medium rounded-lg m-8 p-2 bg-white ">
+        <h3 className="text-center my-4">Upload your image</h3>
+        <p className="text-xs text-gray-400 text-center ">File should be jpeg, png...</p>
+        <div 
+        className={`box-border md:box-content font-medium rounded-lg m-8 p-2 flex flex-col place-items-center border-dashed border-2  ${highlighted ? 'border-green-600 bg-green-100' : 'border-blue-300 bg-blue-50'}`}
+        onDragEnter={() => {
+          setHighlighted(true);
+        }}
+        onDragLeave={() =>{
+          setHighlighted(false);
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          setHighlighted(false);
+          console.log(e.dataTransfer.files);
+          Array.from(e.dataTransfer.files).filter(
+            (file) => file.type === "image/png"
+          ).forEach((file) => {console.log(file)});
+        }}
         >
-          Learn React
-        </a>
-      </header>
+          <br/>
+          <Logo className="object-none"/>
+          <br/>
+          <p className="text-center text-gray-400">Drag & Drop your image here</p>
+          <br/>
+        </div>
+        <p className="text-center text-gray-400">Or</p>
+        <div className="box md:box-content flex justify-center">
+        <button className="text-white text-sm bg-blue-500 rounded-lg py-2 px-4 m-4">Choose a file</button>
+        </div>
+        
+      </div>   
+      
+      <ul>
+        {contacts.map(contact => <li key={contact.email}>
+          <strong>{contact.name}</strong> : {contact.email}
+        </li>)}
+      </ul>
+
     </div>
+
+   
   );
 }
 
